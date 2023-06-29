25e53824f1777b308e91d4cf5c034f865e5abb4d
conno <25connorgriffin@gmail.com>
2023-06-29 13:17:15 -0700

    CSS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import Tesseract from 'tesseract.js';
 
 
 
@@ -68,12 +68,23 @@ export default function Home() {
 
 
   return (
-    <div onPaste={pasteImg} className='bg-slate-900'>
-      <input type="file" accept="image/*" onChange={handleChange} />
+    <div onPaste={pasteImg} className=' w-screen h-screen bg-slate-900'>
+      <div className='flex flex-col justify-center h-1/6 w-full bg-gradient-to-r from-teal-500 to-blue-500 gap-5'>
+          <div>       <div className='font-semibold text-3xl w-screen h-1/24 flex justify-center'> Image Text Extractor</div>
+                   <div className=' text-l w-screen h-1/24 flex justify-center'> Paste an image or choose a file to extract the text out of </div></div> 
+       <div className='h-1/2 w-screen flex justify-center ml-10'> <input type="file" accept="image/*" onChange={handleChange} className='rounded-l opacity-100' /></div>
+      </div>
 
-      {image && <img src={image} alt="Uploaded Image" />}
 
-      {image && <ExtractText image={image} />}
+
+      <div className='w-screen h-fit flex justify-center mt-3'><div className='h-fit w-fit'>{image && <img src={image} alt="Uploaded Image" />}</div></div>
+      
+
+
+      <div className=' mt-3 w-full flex justify-center'>
+    <div className='w-11/12 h-fit bg-white bg-opacity-20 rounded-md text-center '>{image && <ExtractText image={image} />}</div>
+    </div>
+      
 
     </div>
   );
